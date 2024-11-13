4ac98f4a1fb93bc24a978639bdecf9e36a0985be
bautist <i.cola@alumno.etec.um.edu.ar>
2024-11-13 14:09:55 -0300

    cambios nuevos

--- src/routes/creacionUsuarios.js
+++ src/routes/creacionUsuarios.js
@@ -5,38 +5,42 @@ const creacionUsuarios = express.Router();
 
 creacionUsuarios.post('/registrar', registrar);
 creacionUsuarios.post('/iniciar-sesion', iniciarSesion);
-creacionUsuarios.get('/usuarios', listarUsuarios);  
-creacionUsuarios.get('/productos', res.json({
-    products: [
-        {
-          id: 1,
-          imagen: "babolat",
-          nombre: "test jhighjuyy jhjhjkjk uva",
-          descripcion: "Raqueta de alta gama para jugadores profesionales. Test test",
-          precio: 250.00,
-        },
-        {
-          id: 2,
-          imagen: "head",
-          nombre: "Raqueta de Tenis Head",
-          descripcion: "Ideal para jugadores avanzados que buscan control y potencia.",
-          precio: 230.00,
-        },
-        {
-          id: 3,
-          imagen: "wilson",
-          nombre: "Raqueta de Tenis Wilson",
-          descripcion: "Equilibrio perfecto entre potencia y control para todo tipo de jugadores.",
-          precio: 210.00,
-        },
-        {
-          id: 4,
-          imagen: "yonex",
-          nombre: "Raqueta de Tenis Yonex",
-          descripcion: "Raqueta ligera y maniobrable, perfecta para jugadores técnicos.",
-          precio: 240.00,
-        }
-      ]
-}))
+creacionUsuarios.get('/usuarios', listarUsuarios);
+
+// Añade `req` y `res` como parámetros de la función para la ruta /productos
+creacionUsuarios.get('/productos', (req, res) => {
+    res.json({
+        products: [
+            {
+                id: 1,
+                imagen: "babolat",
+                nombre: "test jhighjuyy jhjhjkjk uva",
+                descripcion: "Raqueta de alta gama para jugadores profesionales. Test test",
+                precio: 250.00,
+            },
+            {
+                id: 2,
+                imagen: "head",
+                nombre: "Raqueta de Tenis Head",
+                descripcion: "Ideal para jugadores avanzados que buscan control y potencia.",
+                precio: 230.00,
+            },
+            {
+                id: 3,
+                imagen: "wilson",
+                nombre: "Raqueta de Tenis Wilson",
+                descripcion: "Equilibrio perfecto entre potencia y control para todo tipo de jugadores.",
+                precio: 210.00,
+            },
+            {
+                id: 4,
+                imagen: "yonex",
+                nombre: "Raqueta de Tenis Yonex",
+                descripcion: "Raqueta ligera y maniobrable, perfecta para jugadores técnicos.",
+                precio: 240.00,
+            }
+        ]
+    });
+});
 
 export default creacionUsuarios;
